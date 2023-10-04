d9dc35022a236b8c21595d4cffdf7d0bb45c305e
Donguk Kim <137977991+iridescent0923@users.noreply.github.com>
2023-10-04 09:45:07 +0900

    Before_work

--- C_work/test.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-    printf(" dd");
-
-    return 0;
-}
---
+++ Cpp_Workspace/test.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#define TEXT "TT"
+
+
+using namespace std;
+
+int main()
+{
+    int data;
+    cout <<"dd" <<endl;
+
+    cin >>data;
+
+    cout <<data <<endl;
+
+    return 0;
+
+
+
+}
