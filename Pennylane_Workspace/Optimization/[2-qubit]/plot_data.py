37f78b4f90f5241bbff1888dfd50d65a831a6dd5
Donguk Kim <137977991+iridescent0923@users.noreply.github.com>
2023-11-14 17:45:14 +0900

    before_clamp

--- Pennylane_Workspace/Optimization/[2-qubit]/plot_data.py
+++ Pennylane_Workspace/Optimization/[2-qubit]/plot_data.py
@@ -1,10 +1,15 @@
+# ==============================
+# Standard Library Imports
+# ==============================
 from enum import Enum
 
+# ==============================
+# Third-party Library Imports
+# ==============================
 import numpy as np  # Original numpy
 import matplotlib.pyplot as plt
 from IPython.display import display, Latex
 from matplotlib.ticker import MultipleLocator
-
 
 class Index(Enum):
     PHI = 0
@@ -18,14 +23,21 @@ class Index(Enum):
     TAU_2 = 4
 
 def plot_result(result_data, tau_dephase, gamma_ps_select, object):
-    """ 
-    Plot the results of the optimization.
+    """
+    Plot the results of the optimization based on the specified object type. 
+    It supports plotting Classical Fisher Information (CFI) and the optimized parameters theta_x and phi_z.
 
     Args:
-        result_data (np.ndarray): Data from optimization.
-        tau_dephase (list): List of dephasing rates tau used in optimization.
-        object (str): Type of plot ('CFI', 'theta_x', or 'phi_z').
+        result_data (np.ndarray): Data from the optimization process, including Phi, CFI, and parameters.
+        tau_dephase (list): List of dephasing rates tau used in the optimization.
+        gamma_ps_select (float): The gamma value for post-selection used in the optimization.
+        object (str): Specifies the type of plot to generate. Can be 'CFI', 'theta_x', 'phi_z', 'tau_1', or 'tau_2'.
 
+    The function creates and displays a plot based on the specified object type. 
+    For 'CFI', it plots the Classical Fisher Information over time. 
+    For 'theta_x' and 'phi_z', it plots these parameters over time. 
+    For 'tau_1' and 'tau_2', it plots these tau parameters over time.
+    Each plot includes data for all tau dephasing rates, each represented as a separate line in the plot.
     """
     if object == 'CFI_PS':
         for tau_idx, tau_current in enumerate(tau_dephase):
