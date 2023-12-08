449177493ea2c62163653a9ba1f01d2ee5a2ebdd
Donguk Kim <137977991+iridescent0923@users.noreply.github.com>
2023-12-08 20:22:42 +0900

    [eta]

---
+++ Pennylane_Workspace/Optimization/[1-qubit]/[ps=0.5]/plot_data.py
@@ -0,0 +1,155 @@
+# ==============================
+# Standard Library Imports
+# ==============================
+from enum import Enum
+
+# ==============================
+# Third-party Library Imports
+# ==============================
+import numpy as np  # Original numpy
+import matplotlib.pyplot as plt
+from IPython.display import display, Latex
+from matplotlib.ticker import MultipleLocator
+
+class Index(Enum):
+    PHI = 0
+    CFI = 1
+    
+    PARAS_START = 2
+    THETA_X = 2
+    PHI_Z = 3
+    
+    TAU_1 = 3
+    TAU_2 = 4
+
+def plot_result(result_data, tau_dephase, gamma_ps_select, object):
+    """
+    Plot the results of the optimization based on the specified object type. 
+    It supports plotting Classical Fisher Information (CFI) and the optimized parameters theta_x and phi_z.
+
+    Args:
+        result_data (np.ndarray): Data from the optimization process, including Phi, CFI, and parameters.
+        tau_dephase (list): List of dephasing rates tau used in the optimization.
+        gamma_ps_select (float): The gamma value for post-selection used in the optimization.
+        object (str): Specifies the type of plot to generate. Can be 'CFI', 'theta_x', 'phi_z', 'tau_1', or 'tau_2'.
+
+    The function creates and displays a plot based on the specified object type. 
+    For 'CFI', it plots the Classical Fisher Information over time. 
+    For 'theta_x' and 'phi_z', it plots these parameters over time. 
+    For 'tau_1' and 'tau_2', it plots these tau parameters over time.
+    Each plot includes data for all tau dephasing rates, each represented as a separate line in the plot.
+    """
+    if object == 'CFI_PS':
+        for tau_idx, tau_current in enumerate(tau_dephase):
+            plt.plot(
+                result_data[tau_idx][:,Index.PHI.value], 
+                result_data[tau_idx][:,Index.CFI.value], 
+                label = f'$\\tau$ = {tau_current}'
+            )
+
+        plt.title(f'CFI at $\gamma_{{ps}} = {gamma_ps_select}$')
+        plt.xlabel('Time')
+        plt.ylabel('CFI')
+        plt.grid()
+        plt.legend()
+        plt.show()
+    
+    elif object == 'CFI':
+        for tau_idx, tau_current in enumerate(tau_dephase):
+            plt.plot(
+                result_data[tau_idx][:,Index.PHI.value], 
+                result_data[tau_idx][:,Index.CFI.value], 
+                label = f'$\\tau$ = {tau_current}'
+            )
+            
+        plt.title(f'CFI at $\gamma_{{ps}} = {gamma_ps_select}$')
+        plt.xlabel('Time')
+        plt.ylabel('CFI')
+        plt.grid()
+        plt.legend()
+        plt.show()
+        
+    elif object == 'theta_x':
+        for tau_idx, tau_current in enumerate(tau_dephase):
+            plt.plot(
+                result_data[tau_idx][:,Index.PHI.value], 
+                result_data[tau_idx][:,Index.THETA_X.value], 
+                label = f'$\\tau$ = {tau_current}'
+            )
+            
+        plt.yticks(
+            [-np.pi, -np.pi/2, 0, np.pi/2, np.pi, (3*np.pi)/2, 2*np.pi], 
+            ['$-\pi$', '$-\pi/2$', '0', '$\pi/2$', '$\pi$', '$3\pi/2$', '$2\pi$']
+        )
+        plt.ylim(0, np.pi)
+
+        plt.title(f'Optimized $\\theta_{{x}}$')
+        plt.xlabel('Time')
+        plt.ylabel('RAD')
+        plt.grid()
+        plt.legend()
+        plt.show()
+        
+    elif object == 'phi_z':
+        for tau_idx, tau_current in enumerate(tau_dephase):
+            plt.plot(
+                result_data[tau_idx][:,Index.PHI.value], 
+                result_data[tau_idx][:,Index.PHI_Z.value], 
+                label = f'$\\tau$ = {tau_current}'
+            )
+            
+        plt.yticks(
+            [-np.pi, -np.pi/2, 0, np.pi/2, np.pi, (3*np.pi)/2, 2*np.pi], 
+            ['$-\pi$', '$-\pi/2$', '0', '$\pi/2$', '$\pi$', '$3\pi/2$', '$2\pi$']
+        )
+        # plt.ylim(-np.pi, 2*np.pi)
+
+        plt.title(f'Optimized $\\phi_{{z}}$')
+        plt.xlabel('Time')
+        plt.ylabel('RAD')
+        plt.grid()
+        plt.legend()
+        plt.show()
+        
+    elif object == 'tau_1':
+        for tau_idx, tau_current in enumerate(tau_dephase):
+            plt.plot(
+                result_data[tau_idx][:,Index.PHI.value], 
+                result_data[tau_idx][:,Index.TAU_1.value], 
+                label = f'$\\tau$ = {tau_current}'
+            )
+            
+        plt.yticks(
+            [-np.pi, -np.pi/2, 0, np.pi/2, np.pi, (3*np.pi)/2, 2*np.pi], 
+            ['$-\pi$', '$-\pi/2$', '0', '$\pi/2$', '$\pi$', '$3\pi/2$', '$2\pi$']
+        )
+        plt.ylim(0, np.pi)
+
+        plt.title(f'Optimized $\\tau_{1}$')
+        plt.xlabel('Time')
+        plt.ylabel('RAD')
+        plt.grid()
+        plt.legend()
+        plt.show()
+        
+    elif object == 'tau_2':
+        for tau_idx, tau_current in enumerate(tau_dephase):
+            plt.plot(
+                result_data[tau_idx][:,Index.PHI.value], 
+                result_data[tau_idx][:,Index.TAU_2.value], 
+                label = f'$\\tau$ = {tau_current}'
+            )
+            
+        plt.yticks(
+            [-np.pi, -np.pi/2, 0, np.pi/2, np.pi, (3*np.pi)/2, 2*np.pi], 
+            ['$-\pi$', '$-\pi/2$', '0', '$\pi/2$', '$\pi$', '$3\pi/2$', '$2\pi$']
+        )
+        plt.ylim(0, np.pi)
+
+        plt.title(f'Optimized $\\tau_{2}$')
+        plt.title('Optimized CFI')
+        plt.xlabel('Time')
+        plt.ylabel('RAD')
+        plt.grid()
+        plt.legend()
+        plt.show()
---
+++ Pennylane_Workspace/Optimization/[1-qubit]/[ps=0.5]/torch_optimizer.py
@@ -0,0 +1,49 @@
+import torch
+
+def select_optimizer(method, parameters_in):
+    """
+    Select and configure an optimizer based on the specified method.
+
+    Args:
+        method (str): The optimization method to use ('LBFGS' or 'Adam').
+        parameters_in (torch.Tensor): The parameters to be optimized.
+
+    Returns:
+        torch.optim.Optimizer: Configured optimizer object.
+
+    Raises:
+        ValueError: If an invalid optimization method is specified.
+
+    This function initializes and returns a PyTorch optimizer based on the provided method.
+    For 'LBFGS', it sets up an LBFGS optimizer with specified learning rate, maximum iterations,
+    tolerance levels, and history size. For 'Adam', it sets up an Adam optimizer with specified
+    learning rate, beta values, epsilon for numerical stability, and weight decay options.
+    If a method other than 'LBFGS' or 'Adam' is provided, the function raises a ValueError.
+    """
+    
+    if method == 'LBFGS':
+        opt = torch.optim.LBFGS(
+                [parameters_in], 
+                lr=5e-3,              # Learning rate
+                max_iter=80,          # Maximum number of iterations per optimization step
+                max_eval=None,        # Maximum number of function evaluations per optimization step
+                tolerance_grad=1e-12,  # Termination tolerance on the gradient norm
+                tolerance_change=1e-12,# Termination tolerance on the function value/parameter changes
+                history_size=200,      # Update history size
+                line_search_fn='strong_wolfe'
+        )
+        return opt
+    
+    elif method == 'Adam':
+        opt = torch.optim.Adam(
+            [parameters_in],
+            lr=0.001,                # Learning rate (default: 0.001)
+            betas=(0.9, 0.999),      # Coefficients used for computing running averages of gradient and its square (default: (0.9, 0.999))
+            eps=1e-08,               # Term added to the denominator to improve numerical stability (default: 1e-8)
+            weight_decay=0,          # Weight decay (L2 penalty) (default: 0)
+            amsgrad=False            # Whether to use the AMSGrad variant of this algorithm (default: False)
+        )
+        return opt
+    
+    else:
+        raise ValueError("Invalid optimizer choice.")
